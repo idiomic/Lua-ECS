76e6bdcbd39f24ebb23bab2576ec8871e31aaf9d
Tyler Richard Hoyer <tyler@epicdev.net>
2018-11-12 20:48:19 -0700

    completed and functional

--- ECS.lua
+++ ECS.lua
@@ -1,98 +1,143 @@
+local function erase(relations, key)
+	for _, links in next(relations[key]) do
+		links[key] = nil
+	end
+	relations[key] = nil
+end
+
+local entityToServices = {}
+local entityToStates = {}
+local stateToEntity = {}
+local stateToComponent = {}
+local stateToData = {}
+local componentToStates = {}
+local componentToInit = {}
+local componentToFree = {}
+local serviceToEntities = {}
+local serviceToUpdate = {}
+local serviceToOrder = {}
+local orderToService = {}
+
 local ECS = {}
 
-function ECS:entity_new()
-	local new = #self.entities + 1
-	self.entities[new] = true
-	return new
+-- cannot produce errors
+function ECS.entity_init()
+	local entity = #entityToStates + 1
+	entityToStates[entity] = {}
+	entityToServices[entity] = {}
+	return entity
 end
 
-function ECS:entity_add(entity, componentID)
-	local component = self.components[componentID]
-	component[entity] = component.init(entity)
+-- errors if the entity DNE or is already freed
+function ECS.entity_free(entity)
+	erase(entityToStates, entity)
+	erase(entityToServices, entity)
 end
 
-function ECS:entity_remove(entity, componentID)
-	self.components[componentID][entity] = nil
+-- errors if the entity or component DNE or have been freed
+-- or the component's init callback is not a function
+function ECS.state_init(entity, component, ...)
+	local state = componentToStates[component][entity]
+	if state then
+		stateToData[state] = componentToInit[component](...)
+		return state
+	end
+
+	state = #stateToComponent + 1
+	componentToStates[component][entity] = state
+	entityToStates[entity][component] = state
+
+	stateToComponent[state] = component
+	stateToEntity[state] = entity
+	stateToData[state] = componentToInit[component](...)
+	return state
 end
 
-function ECS:entity_delete(entity)
-	self.entities[entity] = nil
+-- errors if the state DNE or is already freed
+-- or the component's free callback is not a function
+function ECS.state_free(state)
+	local entity = stateToEntity[state]
+	local component = stateToComponent[state]
+	componentToStates[component][entity] = nil
+	entityToStates[entity][component] = nil
+
+	local data = stateToData[state]
+	stateToComponent[state] = nil
+	stateToEntity[state] = nil
+	stateToData[state] = nil
+	componentToFree[component](data)
 end
 
-function ECS:component_new(init)
-	local new = #self.components + 1
-	self.components[new] = { init = init }
-	return new
+-- cannot produce errors
+-- can cause state_init and state_free to error if init and free are not callbacks
+function ECS.component_init(init, free)
+	local component = #componentToInit + 1
+	componentToStates[component] = {}
+	componentToInit[component] = init
+	componentToFree[component] = free
+	return component
 end
 
-function ECS:component_set(componentID, init)
-	self.components[componentID].init = init
+-- errors if the component DNE or is already freed
+function ECS.component_free(component)
+	componentToInit[component] = nil
+	componentToFree[component] = nil
+	erase(componentToStates, component)
 end
 
-function ECS:component_delete(componentID)
-	self.components[componentID] = nil
+-- cannot produce errors
+-- can cause update to error if update isn't a callback
+function ECS.service_init(update)
+	local service = #serviceToEntities + 1
+	serviceToEntities[service] = {}
+	serviceToUpdate[service] = update
+
+	local order = #orderToService + 1
+	orderToService[order] = service
+	serviceToOrder[service] = order
+	return service, order
 end
 
-function ECS:service_new(update)
-	local new = #self.aspects + 1
-	self.aspects[new] = {}
-	self.updates[new] = update
-	return new
+-- errors if the service or entity DNE or have been freed
+function ECS.service_add(service, entity)
+	serviceToEntities[service][entity] = true
+	entityToServices[entity][service] = true
 end
 
-function ECS:service_set(service, update)
-	self.updates[service] = update
+-- errors if the service or entity DNE or have been freed
+function ECS.service_rmv(service, entity)
+	serviceToEntities[service][entity] = nil
+	entityToServices[entity][service] = nil
 end
 
-function ECS:service_add(service, componentID)
-	local aspect = self.aspects[service]
-	aspect[componentID] = true
+-- erros if a service DNE or has been freed
+function ECS.service_swap(service_1, service_2)
+	local order_1 = serviceToOrder[service_1]
+	local order_2 = serviceToOrder[service_2]
+	serviceToOrder[service_1] = order_2
+	orderToService[order_2] = service_1
+	serviceToOrder[service_2] = order_1
+	orderToService[order_1] = service_2
+	return order_1, order_2
 end
 
-function ECS:service_remove(service, componentID)
-	local aspect = self.aspects[service]
-	aspect[componentID] = nil
+-- errors if the service or entity DNE or have been freed
+function ECS.service_free(service)
+	local order = serviceToOrder[service]
+	orderToService[order] = nil
+	serviceToOrder[service] = nil
+	serviceToUpdate[service] = nil
+	erase(serviceToEntities, service)
 end
 
-function ECS:service_delete(service)
-	self.updates[service] = nil
-	self.aspects[service] = nil
-end
-
-function ECS:update()
-	local components = self.components
-	local operations = self.operations
-	local entities = self.entities
-	local updates = self.updates
-	local aspects = self.aspects
-	for operationID = 1, #operations do
-		local service = operations[operationID]
-		local update = updates[service]
-		local aspect = aspects[service]
-		for entity = 1, #entities do
-			local args = {}
-			for componentID in next, aspect do
-				local component = components[componentID][entity]
-				if component == nil then
-					args = nil
-					break
-				end
-				args[componentID] = component
-			end
-			if args then
-				update(args)
-			end
+-- errors if a service's update callback is not a function
+function ECS.update()
+	for order, service in ipairs(orderToService) do
+		local update = serviceToUpdate[service]
+		for entity in next, serviceToEntities[service] do
+			update(entity)
 		end
 	end
 end
 
-ECS.__index = ECS
-return function()
-	return setmetatable({
-		components = {};
-		operations = {};
-		entities = {};
-		updates = {};
-		aspects = {};
-	}, ECS)
-end
+return ECS
